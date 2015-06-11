f39408ea2c29e8ed0145d61079cfc72acffb0e42
Oleksandr Lytvynenko <altvnk@me.com>
2015-06-12 01:13:14 +0300

    Added initial project

---
+++ terraform/openstack/keypair/main.tf
@@ -0,0 +1,20 @@
+variable auth_url {}
+variable tenant_id {}
+variable tenant_name {}
+variable keypair_name {}
+variable public_key {}
+
+provider "openstack" {
+  auth_url      = "${ var.auth_url }"
+  tenant_id     = "${ var.tenant_id }"
+  tenant_name   = "${ var.tenant_name }"
+}
+
+resource "openstack_compute_keypair_v2" "keypair" {
+  name          = "${ var.keypair_name }"
+  public_key    = "${ file(var.public_key) }"
+}
+
+output "keypair_name" {
+	value = "${ openstack_compute_keypair_v2.keypair.name }"
+}
