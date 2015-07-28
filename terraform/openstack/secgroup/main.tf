337ad1782405d3556daad5ad27e0e808bfaf318c
kenjones <kenjones@cisco.com>
2015-07-28 16:31:10 -0400

    updates to formatting, plus add security group specification, and common naming with prefix_name

---
+++ terraform/openstack/secgroup/main.tf
@@ -0,0 +1,43 @@
+variable auth_url {}
+variable tenant_id {}
+variable tenant_name {}
+variable cluster_name {}
+
+provider "openstack" {
+    auth_url = "${ var.auth_url }"
+    tenant_id = "${ var.tenant_id }"
+    tenant_name = "${ var.tenant_name }"
+}
+
+resource "openstack_compute_secgroup_v2" "cluster" {
+    name = "${ var.cluster_name }"
+    description = "Security Group for ${ var.cluster_name }"
+    rule {
+        from_port = 1
+        to_port = 65535
+        ip_protocol = "tcp"
+        self = true
+    }
+    rule {
+        from_port = 1
+        to_port = 65535
+        ip_protocol = "udp"
+        self = true
+    }
+    rule {
+        from_port = 80
+        to_port = 80
+        ip_protocol = "tcp"
+        cidr = "0.0.0.0/0"
+    }
+    rule {
+        from_port = 443
+        to_port = 443
+        ip_protocol = "tcp"
+        cidr = "0.0.0.0/0"
+    }
+}
+
+output "cluster_name" {
+    value = "${ openstack_compute_secgroup_v2.cluster.name }"
+}
