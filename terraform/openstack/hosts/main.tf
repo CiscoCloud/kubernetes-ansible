e5975a189031c3d2eff05aa28274d398c54d8194
shyam <shyashan@cisco.com>
2016-08-08 14:56:25 +0530

    removing code for the creation of additional volumes for cluster

--- terraform/openstack/hosts/main.tf
+++ terraform/openstack/hosts/main.tf
@@ -1,7 +1,6 @@
 variable master_count { }
 variable master_flavor { }
 variable datacenter { default = "openstack" }
-variable glusterfs_volume_size { default = "100" } # size is in gigabytes
 variable image_name { }
 variable keypair_name { }
 variable net_id { }
@@ -30,16 +29,6 @@ resource "template_file" "cloud-init-node" {
   }
 }
 
-resource "openstack_blockstorage_volume_v1" "k8s-glusterfs" {
-  name = "${ var.short_name }-master-glusterfs-${format("%02d", count.index+1) }"
-  description = "${ var.short_name }-master-glusterfs-${format("%02d", count.index+1) }"
-  size = "${ var.glusterfs_volume_size }"
-  metadata = {
-    usage = "container-volumes"
-  }
-  count = "${ var.master_count }"
-}
-
 resource "openstack_compute_instance_v2" "master" {
   name = "${ var.short_name}-master-${format("%02d", count.index+1) }.${ var.host_domain }"
   key_pair = "${ var.keypair_name }"
@@ -47,10 +36,6 @@ resource "openstack_compute_instance_v2" "master" {
   flavor_name = "${ var.master_flavor }"
   security_groups = [ "${ var.security_groups }", "default" ]
   network = { uuid  = "${ var.net_id }" }
-  volume = {
-    volume_id = "${element(openstack_blockstorage_volume_v1.k8s-glusterfs.*.id, count.index)}"
-    device = "/dev/vdb"
-  }
   metadata = {
     dc = "${var.datacenter}"
     role = "master"
